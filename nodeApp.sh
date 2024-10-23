20e488d199428691158c64a5d593e53b569072b4
KhanAyasha <khan.ayas@northeastern.edu>
2024-10-23 06:57:00 -0400

    last commit restored

--- nodeApp.sh
+++ nodeApp.sh
@@ -5,7 +5,7 @@ sudo cp /opt/csye6225/nodeApp.service /etc/systemd/system/
 # Reload systemd to pick up the changes
 sudo systemctl daemon-reload
 
-# sudo systemctl start nodeApp.service
+sudo systemctl start nodeApp.service
 sudo systemctl enable nodeApp.service
 sudo systemctl status nodeApp.service
 
