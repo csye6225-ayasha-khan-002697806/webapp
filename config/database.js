7f499956e9cee7d51f48d55ad6ba6e487fe76eb4
KhanAyasha <khan.ayas@northeastern.edu>
2024-10-23 06:50:09 -0400

    explicitely mentioned DB post to avoid unneccessary append in DB host url

--- config/database.js
+++ config/database.js
@@ -19,6 +19,7 @@ dotenv.config({
 
 const db = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     logging: false
 });
