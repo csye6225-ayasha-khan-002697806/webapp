013e9b0b1640132ccf258651ca297692e65d00ea
KhanAyasha <khan.ayas@northeastern.edu>
2024-10-30 15:29:34 -0400

    cloud watch agent started automatically with systemctl

--- cloudwatch-install.sh
+++ cloudwatch-install.sh
@@ -6,6 +6,9 @@ wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-
 
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 
+sudo systemctl enable amazon-cloudwatch-agent
+sudo systemctl start amazon-cloudwatch-agent
+
 sudo cp /tmp/cloudwatch-config.json /opt/csye6225/cloudwatch-config.json
 sudo chmod 775 /opt/csye6225/cloudwatch-config.json
 cd /opt/csye6225/ && sudo chown csye6225:csye6225 cloudwatch-config.json
@@ -16,3 +19,7 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -m ec2 \
     -c file:/opt/csye6225/cloudwatch-config.json \
     -s
+
+
+
+sudo systemctl restart amazon-cloudwatch-agent
